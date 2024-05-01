078f70e8d65f5ffdef69f39afe97169462f1d4a9
DAgustinAguilera <danielagustinaguilera@gmail.com>
2024-05-01 19:27:42 -0300

    Agregue funcionalidad al boton de trenes regionales de manera momentanea y provicional y ajuste tamaño de fuente

--- script.js
+++ script.js
@@ -4,12 +4,48 @@ const botonalert2 = document.getElementById("boton_alert2");
 const botonalert3 = document.getElementById("boton_alert3");
 const botonTrenesRegionales = document.getElementById("boton-t-r")
 
+
+
+
 botonTrenesRegionales.onclick = ()=>{
+  
   Swal.fire({
-    html: `<div>
-    <img src="/imagenes/Plano mitre.jpeg" alt="">
-    </div>`
+    html: `<div id="map"></div>`,
+    background: `rgba(32, 168, 222, 0.0)`,
+    width: '32em',
+    showCloseButton: true,
+    showConfirmButton: false,
   });
+    var map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: -2 
+    });
+
+    var imageUrl = 'https://i.ibb.co/stCGsYG/planomitre.png';
+    var imageBounds = [[0, 0], [1131, 1131]]; // Dimensiones de la imagen
+    L.imageOverlay(imageUrl, imageBounds, {interactive: false}).addTo(map); // Desactivar la interactividad de la imagen
+
+    map.setMaxBounds(imageBounds);
+
+    map.fitBounds(imageBounds);
+
+    var marker1 = L.marker([856, 1011]).addTo(map)
+    .bindPopup("<b>Punto 1</b><br>Descripción del punto 1.").openPopup();
+
+    var marker2 = L.marker([1011, 278]).addTo(map)
+    .bindPopup("<b>Punto 2</b><br>");
+
+   var marker3 = L.marker([700, 500]).addTo(map)
+    .bindPopup("<b>Punto 3</b><br>Descripción del punto 3.");
+
+    var latlngs = [
+      [1011, 278], // Coordenadas iniciales
+      [968, 278], // Coordenadas finales
+    ];
+    var polyline = L.polyline(latlngs, {color: 'red',weight: 10}).addTo(map);
+
+    // Añadir un popup a la línea
+    polyline.bindPopup("Tramo clausurado por estado de via").openPopup();
 }  
 
 botonalert3.onclick = () =>{
