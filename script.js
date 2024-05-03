baadda5582cd8ddce94c27b702ec7f23cfd0cd37
DAgustinAguilera <danielagustinaguilera@gmail.com>
2024-05-03 16:06:51 -0300

    Agregue boton de firma en nav y modifique mapa de la linea

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const botonalert1 = document.getElementById("boton_alert1");
 const botonalert2 = document.getElementById("boton_alert2");
 const botonalert3 = document.getElementById("boton_alert3");
 const botonTrenesRegionales = document.getElementById("boton-t-r")
+const botonTrenesRegionales1 = document.getElementById("boton-t-r1")
+const botonTrenesRegionales2 = document.getElementById("boton-t-r2")
+const botonTrenesRegionales3 = document.getElementById("boton-t-r3")
+const botonTrenesRegionales4 = document.getElementById("boton-t-r4")
 
 
 
-
-botonTrenesRegionales.onclick = ()=>{
+botonTrenesRegionales4.onclick = ()=>{
   
   Swal.fire({
     html: `<div id="map"></div>`,
@@ -18,10 +21,16 @@ botonTrenesRegionales.onclick = ()=>{
   });
     var map = L.map('map', {
         crs: L.CRS.Simple,
-        minZoom: -2 
+        minZoom: -1 
     });
 
-    var imageUrl = 'https://i.ibb.co/x390sgq/Planomitre-Foto.jpg';
+    var myIcon = L.icon({
+      iconUrl: '/imagenes/caution.png',
+      iconSize: [40 , 40],
+      className: 'iconoalert'
+  });
+
+    var imageUrl = 'https://i.ibb.co/6rVGpdm/planomitre.png';
     var imageBounds = [[0, 0], [1131, 1131]]; // Dimensiones de la imagen
     L.imageOverlay(imageUrl, imageBounds, {interactive: false}).addTo(map); // Desactivar la interactividad de la imagen
 
@@ -29,23 +38,268 @@ botonTrenesRegionales.onclick = ()=>{
 
     map.fitBounds(imageBounds);
 
-    var marker1 = L.marker([856, 1011]).addTo(map)
+    var marker1 = L.marker([856, 1011], {icon: myIcon}).addTo(map)
     .bindPopup("<b>Punto 1</b><br>Descripción del punto 1.").closePopup();
 
-    var marker2 = L.marker([1060, 288]).addTo(map)
+    var marker2 = L.marker([1060, 288],{icon: myIcon}).addTo(map)
     .bindPopup("<b>Punto 2</b><br>");
 
-   var marker3 = L.marker([700, 500]).addTo(map)
+   var marker3 = L.marker([700, 500],{icon: myIcon}).addTo(map)
     .bindPopup("<b>Punto 3</b><br>Descripción del punto 3.");
 
     var latlngs = [
       [1060, 288], // Coordenadas iniciales
-      [1010, 288], // Coordenadas finales
+      [1000, 288], // Coordenadas finales
     ];
-    var polyline = L.polyline(latlngs, {color: 'red',weight: 10}).addTo(map);
+    var polyline = L.polyline(latlngs, {color: 'rgba(255, 0, 0, 0.600)',weight: 10}).addTo(map);
+    
+    var latlngs2 = [
+      [1000, 288], // Coordenadas iniciales
+      [750, 288], // Coordenadas finales
+    ];
+    var polyline2 = L.polyline(latlngs2, {color: 'rgba(0, 128, 0, 0.600)',weight: 10}).addTo(map);
+
 
     // Añadir un popup a la línea
     polyline.bindPopup("Tramo clausurado por estado de via").closePopup();
+    polyline2.bindPopup("Tramo renovado").closePopup();
+
+
+    popup.closePopup();
+}  
+
+
+
+botonTrenesRegionales3.onclick = ()=>{
+  
+  Swal.fire({
+    html: `<div id="map"></div>`,
+    background: `rgba(32, 168, 222, 0.0)`,
+    width: '32em',
+    showCloseButton: true,
+    showConfirmButton: false,
+  });
+    var map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: -1 
+    });
+
+    var myIcon = L.icon({
+      iconUrl: '/imagenes/caution.png',
+      iconSize: [40 , 40],
+      className: 'iconoalert'
+  });
+
+    var imageUrl = 'https://i.ibb.co/6rVGpdm/planomitre.png';
+    var imageBounds = [[0, 0], [1131, 1131]]; // Dimensiones de la imagen
+    L.imageOverlay(imageUrl, imageBounds, {interactive: false}).addTo(map); // Desactivar la interactividad de la imagen
+
+    map.setMaxBounds(imageBounds);
+
+    map.fitBounds(imageBounds);
+
+    var marker1 = L.marker([856, 1011], {icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 1</b><br>Descripción del punto 1.").closePopup();
+
+    var marker2 = L.marker([1060, 288],{icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 2</b><br>");
+
+   var marker3 = L.marker([700, 500],{icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 3</b><br>Descripción del punto 3.");
+
+    var latlngs = [
+      [1060, 288], // Coordenadas iniciales
+      [1000, 288], // Coordenadas finales
+    ];
+    var polyline = L.polyline(latlngs, {color: 'rgba(255, 0, 0, 0.600)',weight: 10}).addTo(map);
+    
+    var latlngs2 = [
+      [1000, 288], // Coordenadas iniciales
+      [750, 288], // Coordenadas finales
+    ];
+    var polyline2 = L.polyline(latlngs2, {color: 'rgba(0, 128, 0, 0.600)',weight: 10}).addTo(map);
+
+
+    // Añadir un popup a la línea
+    polyline.bindPopup("Tramo clausurado por estado de via").closePopup();
+    polyline2.bindPopup("Tramo renovado").closePopup();
+
+
+    popup.closePopup();
+}  
+
+
+
+botonTrenesRegionales2.onclick = ()=>{
+  
+  Swal.fire({
+    html: `<div id="map"></div>`,
+    background: `rgba(32, 168, 222, 0.0)`,
+    width: '32em',
+    showCloseButton: true,
+    showConfirmButton: false,
+  });
+    var map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: -1 
+    });
+
+    var myIcon = L.icon({
+      iconUrl: '/imagenes/caution.png',
+      iconSize: [40 , 40],
+      className: 'iconoalert'
+  });
+
+    var imageUrl = 'https://i.ibb.co/6rVGpdm/planomitre.png';
+    var imageBounds = [[0, 0], [1131, 1131]]; // Dimensiones de la imagen
+    L.imageOverlay(imageUrl, imageBounds, {interactive: false}).addTo(map); // Desactivar la interactividad de la imagen
+
+    map.setMaxBounds(imageBounds);
+
+    map.fitBounds(imageBounds);
+
+    var marker1 = L.marker([856, 1011], {icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 1</b><br>Descripción del punto 1.").closePopup();
+
+    var marker2 = L.marker([1060, 288],{icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 2</b><br>");
+
+   var marker3 = L.marker([700, 500],{icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 3</b><br>Descripción del punto 3.");
+
+    var latlngs = [
+      [1060, 288], // Coordenadas iniciales
+      [1000, 288], // Coordenadas finales
+    ];
+    var polyline = L.polyline(latlngs, {color: 'rgba(255, 0, 0, 0.600)',weight: 10}).addTo(map);
+    
+    var latlngs2 = [
+      [1000, 288], // Coordenadas iniciales
+      [750, 288], // Coordenadas finales
+    ];
+    var polyline2 = L.polyline(latlngs2, {color: 'rgba(0, 128, 0, 0.600)',weight: 10}).addTo(map);
+
+
+    // Añadir un popup a la línea
+    polyline.bindPopup("Tramo clausurado por estado de via").closePopup();
+    polyline2.bindPopup("Tramo renovado").closePopup();
+
+
+    popup.closePopup();
+}  
+
+botonTrenesRegionales1.onclick = ()=>{
+  
+  Swal.fire({
+    html: `<div id="map"></div>`,
+    background: `rgba(32, 168, 222, 0.0)`,
+    width: '32em',
+    showCloseButton: true,
+    showConfirmButton: false,
+  });
+    var map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: -1
+    });
+
+    var myIcon = L.icon({
+      iconUrl: '/imagenes/caution.png',
+      iconSize: [40 , 40],
+      className: 'iconoalert'
+  });
+
+    var imageUrl = 'https://i.ibb.co/6rVGpdm/planomitre.png';
+    var imageBounds = [[0, 0], [1131, 1131]]; // Dimensiones de la imagen
+    L.imageOverlay(imageUrl, imageBounds, {interactive: false}).addTo(map); // Desactivar la interactividad de la imagen
+
+    map.setMaxBounds(imageBounds);
+
+    map.fitBounds(imageBounds);
+
+    var marker1 = L.marker([856, 1011], {icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 1</b><br>Descripción del punto 1.").closePopup();
+
+    var marker2 = L.marker([1060, 288],{icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 2</b><br>");
+
+   var marker3 = L.marker([700, 500],{icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 3</b><br>Descripción del punto 3.");
+
+    var latlngs = [
+      [1060, 288], // Coordenadas iniciales
+      [1000, 288], // Coordenadas finales
+    ];
+    var polyline = L.polyline(latlngs, {color: 'rgba(255, 0, 0, 0.600)',weight: 10}).addTo(map);
+    
+    var latlngs2 = [
+      [1000, 288], // Coordenadas iniciales
+      [750, 288], // Coordenadas finales
+    ];
+    var polyline2 = L.polyline(latlngs2, {color: 'rgba(0, 128, 0, 0.600)',weight: 10}).addTo(map);
+
+
+    // Añadir un popup a la línea
+    polyline.bindPopup("Tramo clausurado por estado de via").closePopup();
+    polyline2.bindPopup("Tramo renovado").closePopup();
+
+
+    popup.closePopup();
+}  
+
+botonTrenesRegionales.onclick = ()=>{
+  
+  Swal.fire({
+    html: `<div id="map"></div>`,
+    background: `rgba(32, 168, 222, 0.0)`,
+    width: '35em',
+    showCloseButton: true,
+    showConfirmButton: false,
+  });
+    var map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: -1.5
+    });
+
+    var myIcon = L.icon({
+      iconUrl: '/imagenes/caution.png',
+      iconSize: [40 , 40],
+      className: 'iconoalert'
+  });
+
+    var imageUrl = 'https://i.ibb.co/6rVGpdm/planomitre.png';
+    var imageBounds = [[0, 0], [1131, 1131]]; // Dimensiones de la imagen
+    L.imageOverlay(imageUrl, imageBounds, {interactive: false}).addTo(map); // Desactivar la interactividad de la imagen
+
+    map.setMaxBounds(imageBounds);
+
+    map.fitBounds(imageBounds);
+
+    var marker1 = L.marker([856, 1011], {icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 1</b><br>Descripción del punto 1.").closePopup();
+
+    var marker2 = L.marker([1060, 288],{icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 2</b><br>");
+
+   var marker3 = L.marker([700, 500],{icon: myIcon}).addTo(map)
+    .bindPopup("<b>Punto 3</b><br>Descripción del punto 3.");
+
+    var latlngs = [
+      [1060, 288], // Coordenadas iniciales
+      [1000, 288], // Coordenadas finales
+    ];
+    var polyline = L.polyline(latlngs, {color: 'rgba(255, 0, 0, 0.600)',weight: 10}).addTo(map);
+    
+    var latlngs2 = [
+      [1000, 288], // Coordenadas iniciales
+      [750, 288], // Coordenadas finales
+    ];
+    var polyline2 = L.polyline(latlngs2, {color: 'rgba(0, 128, 0, 0.600)',weight: 10}).addTo(map);
+
+
+    // Añadir un popup a la línea
+    polyline.bindPopup("Tramo clausurado por estado de via").closePopup();
+    polyline2.bindPopup("Tramo renovado").closePopup();
+
 
     popup.closePopup();
 }  
